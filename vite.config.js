c80a8d76676434a0c56d56619b4e88caa0d97fc1
sivayaburka <sivkov1998@yandex.ru>
2024-12-07 12:27:09 +0300

    added base for vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 const path = require("path");
 import vue from "@vitejs/plugin-vue";
 
+const ASSET_URL = 'bootstrap_5'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
@@ -9,5 +11,15 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // lodash: ['lodash']
+        }
+      }
+    },
+  },
   plugins: [vue()],
+   base: `/${ASSET_URL}/`
 });
